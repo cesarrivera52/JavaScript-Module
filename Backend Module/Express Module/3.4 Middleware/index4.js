f75eb49f72717bfc1244802af28c4677940ae263
César Alejandro Rivera Guzmán <117124512+cesarrivera52@users.noreply.github.com>
2026-04-27 23:44:12 -0600

    Add files via upload

---
+++ Backend Module/Express Module/3.4 Middleware/index4.js
@@ -0,0 +1,37 @@
+//Libraries
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+var bandNameStreet = "";
+var bandNamePet = "";
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+function bandNameGenerator (req, res, next){
+  console.log(req.body);
+  bandNameStreet = req.body["street"];
+  bandNamePet = req.body["pet"];
+  next();
+};
+app.use(bandNameGenerator);
+
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/submit", (req, res) => {
+  res.send(`<h1>Your band name is: </h1><h2>${bandNameStreet} ${bandNamePet}</h2>`);
+}); 
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
+
